25a14daab7e42b1a9f0298d28bb6bc4abd2a3501
henryhaller99 <HenryHernandezBarajas@outlook.com>
2020-08-24 17:53:43 -0500

    Card with a child Container

--- lib/main.dart
+++ lib/main.dart
@@ -16,12 +16,22 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter App'),
-      ),
-      body: Center(
-        child: Text('Widget Playground!'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Flutter App'),
+        ),
+        body: Column(
+          children: [
+            Card(
+              color: Colors.blue  ,
+              child: Container(
+                child: Text('Chart'),
+              ),
+              elevation: 5,
+            ),
+            Card(
+              child: Text('List of tx'),
+            ),
+          ],
+        ));
   }
 }
